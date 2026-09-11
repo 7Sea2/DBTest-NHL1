9974d9bf4283c92da1081b69eeac3d30a4f4ce80
7Sea2 <joshuaidsardi@gmail.com>
2026-09-11 11:42:44 +0200

    Initial Commit

---
+++ DBProject Eddie/Program.cs
@@ -0,0 +1,30 @@
+﻿using MySqlConnector;
+
+Console.WriteLine("Hello, World!");
+
+
+string connectionString = "Server=localhost;Database=company;Port=3306;Uid=root;Pwd=1234;"; 
+using MySqlConnection connection = new MySqlConnection(connectionString);
+
+string sqlRead = """
+                 SELECT d.name, e.firstname
+                 FROM company.department d
+                          JOIN company.employees e ON d.idDepartment = e.idEmployee
+                 """;
+
+using MySqlCommand command = new MySqlCommand(sqlRead, connection);
+{
+    connection.Open();
+    using MySqlDataReader reader = command.ExecuteReader();
+    {
+
+        while (reader.Read())
+        {
+            string name = reader.GetString(name: "name");
+            string firstname = reader.GetString(name: "firstname");
+            Console.WriteLine($"{name} - {firstname}");
+        }
+    }
+}
+
+connection.Close();
