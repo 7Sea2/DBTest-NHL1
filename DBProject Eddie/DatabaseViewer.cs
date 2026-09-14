1eef09f52372898e6d095452b705b604e18d19c8
7Sea2 <joshuaidsardi@gmail.com>
2026-09-14 11:43:12 +0200

    Create and Fill works

---
+++ DBProject Eddie/DatabaseViewer.cs
@@ -0,0 +1,33 @@
+﻿namespace DBProject_Eddie;
+using MySqlConnector;
+
+public class DatabaseViewer
+{
+    private readonly string connectionString =
+        "Server=localhost;Database=company;Port=3306;Uid=root1;Pwd=1234;";
+
+    public void ShowDatabase()
+    {
+        string sqlRead = """
+                         SELECT d.name, e.firstname
+                         FROM company.department d
+                         JOIN company.employees e 
+                             ON d.idDepartment = e.idEmployee
+                         """;
+
+        using MySqlConnection connection = new MySqlConnection(connectionString);
+        using MySqlCommand command = new MySqlCommand(sqlRead, connection);
+
+        connection.Open();
+
+        using MySqlDataReader reader = command.ExecuteReader();
+
+        while (reader.Read())
+        {
+            string department = reader.GetString("name");
+            string firstname = reader.GetString("firstname");
+
+            Console.WriteLine($"{department} - {firstname}");
+        }
+    }
+}
--- DBProject Eddie/Program.cs
+++ DBProject Eddie/Program.cs
@@ -1,30 +1,77 @@
-﻿using MySqlConnector;
+﻿using DBProject_Eddie;
+using MySqlConnector;
 
-Console.WriteLine("Hello, World!");
+// Connect aan de andere scripts in-file
+DatabaseManager databaseManager = new DatabaseManager();
+DatabaseViewer databaseViewer = new DatabaseViewer();
 
+// Bool voor afsluiten
+bool running = true;
 
-string connectionString = "Server=localhost;Database=company;Port=3306;Uid=root;Pwd=1234;"; 
-using MySqlConnection connection = new MySqlConnection(connectionString);
-
-string sqlRead = """
-                 SELECT d.name, e.firstname
-                 FROM company.department d
-                          JOIN company.employees e ON d.idDepartment = e.idEmployee
-                 """;
-
-using MySqlCommand command = new MySqlCommand(sqlRead, connection);
+// Main Menu
+while (running)
 {
-    connection.Open();
-    using MySqlDataReader reader = command.ExecuteReader();
-    {
+    Console.Clear();
 
-        while (reader.Read())
-        {
-            string name = reader.GetString(name: "name");
-            string firstname = reader.GetString(name: "firstname");
-            Console.WriteLine($"{name} - {firstname}");
-        }
+    Console.WriteLine("--Main Menu");
+    Console.WriteLine("1. Database Aanmaken");
+    Console.WriteLine("1. Vul Database");
+    Console.WriteLine("3. Database Viewer");
+    Console.WriteLine("4. Entry Aanpassen");
+    Console.WriteLine("5. Entry Verwijderen");
+    Console.WriteLine("6. ASCII");
+    Console.WriteLine("7. Exit");
+    Console.Write("Keuze: ");
+
+    string? choice = Console.ReadLine();
+
+    switch (choice)
+    {
+        case "1":
+            Console.Clear();
+            databaseManager.CreateOrFillDatabase(true);
+            break;
+        
+        case "2":
+            Console.Clear();
+            databaseManager.CreateOrFillDatabase(false);
+            break;
+
+        case "3":
+            Console.Clear();
+            databaseViewer.ShowDatabase();
+            break;
+
+        case "4":
+            Console.Clear();
+            Console.WriteLine("Not Implemented");
+            break;
+
+        case "5":
+            Console.Clear();
+            Console.WriteLine("Not Implemented");
+            break;
+
+        case "6":
+            Console.Clear();
+            Console.WriteLine("Not Implemented");
+            break;
+
+        case "7":
+            Console.Clear();
+            running = false;
+            Console.WriteLine("Programma wordt afgesloten.");
+            break;
+
+        default:
+            Console.WriteLine("Ongeldige keuze.");
+            break;
+    }
+
+    if (running)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Druk op Enter om door te gaan.");
+        Console.ReadLine();
     }
 }
-
-connection.Close();
